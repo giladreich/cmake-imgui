972de286f074a5b26384bc7a01fcc1c1c269b08d
KiwifruitDev <teampopplio@protonmail.com>
2022-08-23 14:47:17 -0700

    Update imgui submodule to latest master (v1.88)

--- CMakeImplFiles.cmake
+++ CMakeImplFiles.cmake
@@ -8,49 +8,49 @@ function(Init_Impl_Files HEADERS_CXX_IMPL_FILES SOURCES_CXX_IMPL_FILES)
   if (IMGUI_IMPL_DX9)
     message(STATUS "[INFO] Including Direct3D9 implementation files.")
     set(HEADERS_CXX_IMPL_FILES
-      ${EXAMPLES_DIR}/imgui_impl_dx9.h
-      ${EXAMPLES_DIR}/imgui_impl_win32.h
+      ${BACKENDS_DIR}/imgui_impl_dx9.h
+      ${BACKENDS_DIR}/imgui_impl_win32.h
       PARENT_SCOPE
     )
     set(SOURCES_CXX_IMPL_FILES
-      ${EXAMPLES_DIR}/imgui_impl_dx9.cpp
-      ${EXAMPLES_DIR}/imgui_impl_win32.cpp
+      ${BACKENDS_DIR}/imgui_impl_dx9.cpp
+      ${BACKENDS_DIR}/imgui_impl_win32.cpp
       PARENT_SCOPE
     )
   elseif(IMGUI_IMPL_DX10)
     message(STATUS "[INFO] Including Direct3D10 implementation files.")
     set(HEADERS_CXX_IMPL_FILES
-      ${EXAMPLES_DIR}/imgui_impl_dx10.h
-      ${EXAMPLES_DIR}/imgui_impl_win32.h
+      ${BACKENDS_DIR}/imgui_impl_dx10.h
+      ${BACKENDS_DIR}/imgui_impl_win32.h
       PARENT_SCOPE
     )
     set(SOURCES_CXX_IMPL_FILES
-      ${EXAMPLES_DIR}/imgui_impl_dx10.cpp
-      ${EXAMPLES_DIR}/imgui_impl_win32.cpp
+      ${BACKENDS_DIR}/imgui_impl_dx10.cpp
+      ${BACKENDS_DIR}/imgui_impl_win32.cpp
       PARENT_SCOPE
     )
   elseif(IMGUI_IMPL_DX11)
     message(STATUS "[INFO] Including Direct3D11 implementation files.")
     set(HEADERS_CXX_IMPL_FILES
-      ${EXAMPLES_DIR}/imgui_impl_dx11.h
-      ${EXAMPLES_DIR}/imgui_impl_win32.h
+      ${BACKENDS_DIR}/imgui_impl_dx11.h
+      ${BACKENDS_DIR}/imgui_impl_win32.h
       PARENT_SCOPE
     )
     set(SOURCES_CXX_IMPL_FILES
-      ${EXAMPLES_DIR}/imgui_impl_dx11.cpp
-      ${EXAMPLES_DIR}/imgui_impl_win32.cpp
+      ${BACKENDS_DIR}/imgui_impl_dx11.cpp
+      ${BACKENDS_DIR}/imgui_impl_win32.cpp
       PARENT_SCOPE
     )
   elseif(IMGUI_IMPL_DX12)
     message(STATUS "[INFO] Including Direct3D12 implementation files.")
     set(HEADERS_CXX_IMPL_FILES
-      ${EXAMPLES_DIR}/imgui_impl_dx12.h
-      ${EXAMPLES_DIR}/imgui_impl_win32.h
+      ${BACKENDS_DIR}/imgui_impl_dx12.h
+      ${BACKENDS_DIR}/imgui_impl_win32.h
       PARENT_SCOPE
     )
     set(SOURCES_CXX_IMPL_FILES
-      ${EXAMPLES_DIR}/imgui_impl_dx12.cpp
-      ${EXAMPLES_DIR}/imgui_impl_win32.cpp
+      ${BACKENDS_DIR}/imgui_impl_dx12.cpp
+      ${BACKENDS_DIR}/imgui_impl_win32.cpp
       PARENT_SCOPE
     )
   else()
